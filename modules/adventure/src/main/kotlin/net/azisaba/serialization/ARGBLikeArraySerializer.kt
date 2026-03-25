42e84afdf7c7d3d1e45ad9381b333ab863a4bfe2
tksimeji <tksimeji@outlook.com>
2026-03-25 09:33:01 +0900

    feat: add array serializers for net.kyori.adventure.util.RGBLike and net.kyori.adventure.ARGBLike

---
+++ modules/adventure/src/main/kotlin/net/azisaba/serialization/ARGBLikeArraySerializer.kt
@@ -0,0 +1,68 @@
+package net.azisaba.serialization
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.buildSerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.decodeStructure
+import net.kyori.adventure.text.format.ShadowColor
+import net.kyori.adventure.util.ARGBLike
+
+@OptIn(InternalSerializationApi::class)
+object ARGBLikeArraySerializer : KSerializer<ARGBLike> {
+    override val descriptor: SerialDescriptor = buildSerialDescriptor("ARGBLikeArray", StructureKind.LIST)
+
+    override fun serialize(encoder: Encoder, value: ARGBLike) {
+        val red = value.red()
+        val green = value.green()
+        val blue = value.blue()
+        val alpha = value.alpha()
+
+        require(red in 0..255 && green in 0..255 && blue in 0..255 && alpha in 0..255) {
+            "ARGB value must be between 0 and 255"
+        }
+
+        val composite = encoder.beginCollection(descriptor, 4)
+        composite.encodeIntElement(descriptor, 0, red)
+        composite.encodeIntElement(descriptor, 1, green)
+        composite.encodeIntElement(descriptor, 2, blue)
+        composite.encodeIntElement(descriptor, 3, alpha)
+        composite.endStructure(descriptor)
+    }
+
+    override fun deserialize(decoder: Decoder): ARGBLike {
+        var red: Int? = null
+        var green: Int? = null
+        var blue: Int? = null
+        var alpha: Int? = null
+
+        decoder.decodeStructure(descriptor) {
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> red = decodeIntElement(descriptor, 0)
+                    1 -> green = decodeIntElement(descriptor, 1)
+                    2 -> blue = decodeIntElement(descriptor, 2)
+                    3 -> alpha = decodeIntElement(descriptor, 3)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> throw SerializationException("ARGBLikeArray index must be between 0 and 3, got: $index")
+                }
+            }
+        }
+
+        val r = red ?: throw SerializationException("ARGBLikeArray must contain red")
+        val g = green ?: throw SerializationException("ARGBLikeArray must contain green")
+        val b = blue ?: throw SerializationException("ARGBLikeArray must contain blue")
+        val a = alpha ?: throw SerializationException("ARGBLikeArray must contain alpha")
+
+        require(r in 0..255 && g in 0..255 && b in 0..255 && a in 0..255) {
+            "ARGB value must be between 0 and 255"
+        }
+
+        return ShadowColor.shadowColor(r, g, b, a)
+    }
+}
---
+++ modules/adventure/src/main/kotlin/net/azisaba/serialization/RGBLikeArraySerializer.kt
@@ -0,0 +1,63 @@
+package net.azisaba.serialization
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.buildSerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.decodeStructure
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.util.RGBLike
+
+@OptIn(InternalSerializationApi::class)
+object RGBLikeArraySerializer : KSerializer<RGBLike> {
+    override val descriptor: SerialDescriptor = buildSerialDescriptor("RGBLikeArray", StructureKind.LIST)
+
+    override fun serialize(encoder: Encoder, value: RGBLike) {
+        val red = value.red()
+        val green = value.green()
+        val blue = value.blue()
+
+        require(red in 0..255 && green in 0..255 && blue in 0..255) {
+            "RGB value must be between 0 and 255"
+        }
+
+        val composite = encoder.beginCollection(descriptor, 3)
+        composite.encodeIntElement(descriptor, 0, red)
+        composite.encodeIntElement(descriptor, 1, green)
+        composite.encodeIntElement(descriptor, 2, blue)
+        composite.endStructure(descriptor)
+    }
+
+    override fun deserialize(decoder: Decoder): RGBLike {
+        var red: Int? = null
+        var green: Int? = null
+        var blue: Int? = null
+
+        decoder.decodeStructure(descriptor) {
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> red = decodeIntElement(descriptor, 0)
+                    1 -> green = decodeIntElement(descriptor, 1)
+                    2 -> blue = decodeIntElement(descriptor, 2)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> throw SerializationException("RGBLikeArray index must be between 0 and 2, got: $index")
+                }
+            }
+        }
+
+        val r = red ?: throw SerializationException("RGBLikeArray must contain red")
+        val g = green ?: throw SerializationException("RGBLikeArray must contain green")
+        val b = blue ?: throw SerializationException("RGBLikeArray must contain blue")
+
+        require(r in 0..255 && g in 0..255 && b in 0..255) {
+            "RGB value must be between 0 and 255"
+        }
+
+        return TextColor.color(r, g, b)
+    }
+}
